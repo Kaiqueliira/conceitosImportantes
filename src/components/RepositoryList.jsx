e0bfd883ad45d2ccf2d978688674760196287675
Kaique lira <kaiqueliira@gmail.com>
2021-11-04 12:06:36 -0300

    Adicionado Componente de lista de Repositorios

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,17 @@
+export function RepositoryList() {
+    return (
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>UHuhuhhu</strong>
+                    <p>
+                        Repositorio UAHSUHASU
+                    </p>
+                    <a href="#">Acessar Repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
